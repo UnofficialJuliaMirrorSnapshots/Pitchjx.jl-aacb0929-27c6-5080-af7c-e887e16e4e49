7b4863aada7690a577154267fdb8435216e442ec
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-08 01:44:22 +0000

    Snapshot of branch develop taken on 2019-05-07T21:44:21.852-04:00 from "https://github.com/UnofficialJuliaMirror/Pitchjx.jl-aacb0929-27c6-5080-af7c-e887e16e4e49"

--- src/extractor.jl
+++ src/extractor.jl
@@ -1,35 +1,38 @@
+using HTTP
 using EzXML
 using DataFrames
 using Dates
 
 function extract(date)
     result = DataFrame(
-        date=String[],
-        pitcherid=String[],
-        pitcher_teamid=String[],
-        pitcher_firstname=String[],
-        pitcher_lastname=String[],
-        pitcher_teamname=String[],
-        pitcherthrow=String[],
-        batterid=String[],
-        batter_teamid=String[],
-        batter_firstname=String[],
-        batter_lastname=String[],
-        batter_teamname=String[],
-        batterstand=String[],
-        eventdesc=String[],
-        pitchresult=String[],
-        x=String[],
-        y=String[],
-        px=String[],
-        pz=String[],
-        sztop=String[],
-        szbottom=String[],
-        pitchtype=String[],
-        startspeed=String[],
-        endspeed=String[],
-        spindir=String[],
-        spinrate=String[]
+        date = String[],
+        pitcherid = String[],
+        pitcher_teamid = String[],
+        pitcher_firstname = String[],
+        pitcher_lastname = String[],
+        pitcher_teamname = String[],
+        pitcherthrow = String[],
+        batterid = String[],
+        batter_teamid = String[],
+        batter_firstname = String[],
+        batter_lastname = String[],
+        batter_teamname = String[],
+        batterstand = String[],
+        eventdesc = String[],
+        pitchresult = String[],
+        x = String[],
+        y = String[],
+        px = String[],
+        pz = String[],
+        zone = String[],
+        sztop = String[],
+        szbottom = String[],
+        pitchtype = String[],
+        startspeed = String[],
+        endspeed = String[],
+        spindir = String[],
+        spinrate = String[],
+        nasty = String[]
     )
     year = Dates.year(date)
     month = lpad(Dates.month(date), 2, "0")
@@ -76,19 +79,39 @@ function extract(date)
                     :y,
                     :px,
                     :pz,
+                    :zone,
                     :sztop,
                     :szbottom,
                     :pitchtype,
                     :startspeed,
                     :endspeed,
                     :spindir,
-                    :spinrate
+                    :spinrate,
+                    :nasty
                 ]
             ])
             @info "Process dataset: " date "/" litext ": Finish!"
         end
     end
     return result
+end
+
+function gethtml(url)
+    r = HTTP.get(url)
+    if 200 <= r.status < 300
+        return root(parsehtml(String(r.body)))
+    else
+        error("Page is not accessable.")
+    end
+end
+
+function getxml(url)
+    r = HTTP.get(url)
+    if 200 <= r.status < 300
+        return root(parsexml(String(r.body)))
+    else
+        error("Page is not accessable.")
+    end
 end
 
 function getplayers(base, gid, date)
@@ -130,13 +153,15 @@ function getpitches(base, gid, date)
         y = String[],
         px = String[],
         pz = String[],
+        zone = String[],
         sztop = String[],
         szbottom = String[],
         pitchtype = String[],
         startspeed = String[],
         endspeed = String[],
         spindir = String[],
-        spinrate = String[]
+        spinrate = String[],
+        nasty = String[]
     )
     url = base * "/" * gid * "inning/inning_all.xml"
     pitches = getxml(url)
@@ -146,13 +171,14 @@ function getpitches(base, gid, date)
         pitcherthrow = atbat["p_throws"]
         batterstand = atbat["stand"]
         eventdesc = atbat["des"]
-        for pitch in findall("//pitch", atbat)
+        for pitch in findall("./pitch", atbat)
             try
                 pitchresult = pitch["des"]
                 x = pitch["x"]
                 y = pitch["y"]
                 px = pitch["px"]
                 pz = pitch["pz"]
+                zone = pitch["zone"]
                 sztop = pitch["sz_top"]
                 szbottom = pitch["sz_bot"]
                 pitchtype = pitch["pitch_type"]
@@ -160,6 +186,7 @@ function getpitches(base, gid, date)
                 endspeed = pitch["end_speed"]
                 spindir = pitch["spin_dir"]
                 spinrate = pitch["spin_rate"]
+                nasty = pitch["nasty"]
                 push!(pitchdf, [
                     Dates.format(date, "yyyy-mm-dd"),
                     pitcherid,
@@ -172,13 +199,15 @@ function getpitches(base, gid, date)
                     y,
                     px,
                     pz,
+                    zone,
                     sztop,
                     szbottom,
                     pitchtype,
                     startspeed,
                     endspeed,
                     spindir,
-                    spinrate
+                    spinrate,
+                    nasty
                     ]
                 )
             catch
